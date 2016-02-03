464da31a4a2885c8823f19991f6e96b7455936b2
squez <squez@users.noreply.github.com>
2016-02-02 23:41:07 -0600

    accidentally deleted lang files...

---
+++ inc/languages/english/minecraftconnect.lang.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+* Minecraft Connect English lang file
+**/
+$l['mcc'] = 'Minecraft Connect';
+
+// UserCP
+$l['mcc_usercpnav'] = 'Minecraft Connect'; // UserCP usercp nav list 
+$l['mcc_usercp_menu_title'] = 'Minecraft Connect';
+$l['mcc_usercp_title'] = 'Minecraft Connect';
+$l['mcc_link'] = 'Link Account';
+$l['mcc_unlink'] = 'Unlink Account';
+$l['mcc_username'] = 'Minecraft Username/Email';
+$l['mcc_id'] = 'Minecraft ID';
+$l['mcc_password'] = 'Minecraft Password';
+$l['mcc_usercp_err'] = 'Invalid link/unlink request.';
+$l['mcc_usercp_link_title'] = 'Minecraft Connect - Link Account';
+$l['mcc_usercp_link_err'] = 'Invalid account link request.';
+$l['mcc_already_linked'] = 'You already have a Minecraft account connected to your MyBB profile.';
+$l['mcc_link_heading'] = 'Enter your Minecraft account information to authenticate with Mojang servers. Your password will never be stored on this site.';
